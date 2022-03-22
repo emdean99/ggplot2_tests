7c69d52c10e40b886660961f895e76cbf2dc24fb
emdean99 <deanethan3@gmail.com>
2022-03-22 16:31:01 -0400

    started video on understanding basics of ggplot, rest I hopefully can pickup

--- ggplot2_test.R
+++ ggplot2_test.R
@@ -39,17 +39,34 @@ ggplot(BOD, aes(Time, demand)) +
 
 
 # Different graph with different Dataset
+# Thi could be perticularily helpful with the errors I am having with the budget
+# Information
+# I believe because of how I have my dataframe setup (need to remove the missing
+# characters) having no column names for it to read as an X or label for the data
+# It is throwing errors, this is similar to what I was seeing with the co2 vs CO2
+
 
 data()
 
-view(co2)
+view(CO2)
 
-co22 <- as.data.frame(co2)
 
 # Graph
 
-ggplot(co2)
+ggplot(CO2)
 
 #orrrr using the pipe operator (because of tidyverse)
-co22 %>%
-  ggplot(aes(conc, update, color = Treatment)) + geom_point()
+# alpha makes it transparent
+# smooth creates the linear model that follows it with the standard error
+# set the linear model with method and the se to not show with F
+# Changes the type to show with the facet wrap (splits it up)
+
+
+CO2 %>%
+  ggplot(aes(conc, uptake, color = Treatment)) +
+  geom_point(size = 5, alpha = 0.5) + 
+  geom_smooth(method = lm, se = F) + 
+  facet_wrap(~Type)
+
+
+# Finished video at 13:36 for the day
